497aca99a973afbb3bfa9f4cec7e5f938e5a435e
Tyr Zong <tianyizong@gmail.com>
2023-07-27 00:16:07 -0400

    collapsible nav bar, need more work

---
+++ js/scripts-dist.js
@@ -0,0 +1,1 @@
+function showNav(){for(var e=document.getElementsByClassName("nav-item"),s=0;s<e.length;s++)"grid"==e[s].style.display?e[s].style.display="none":e[s].style.display="grid"}
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function showNav(){
+    var navItems = document.getElementsByClassName("nav-item")
+    for (var i = 0; i<navItems.length; i++){
+        if (navItems[i].style.display == "grid"){
+            navItems[i].style.display = "none";
+        }
+        else{
+            navItems[i].style.display = "grid";
+        }   
+    }
+}
