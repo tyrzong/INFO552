6f1824a93699ab2cb9cc3eee9aa557b49db4b22a
Tyr Zong <tianyizong@gmail.com>
2023-08-24 20:53:57 -0400

    finished calculator

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ function toggleNav(){
         }
     }
 }
+
+function runCalc(){
+    let lower = document.getElementById("lower").value;
+    let higher = document.getElementById("higher").value;
+    let gallons = document.getElementById("gallons").value;
+    document.getElementById("loweroctane").innerHTML = lower;
+    document.getElementById("higheroctane").innerHTML = higher;
+    let higherneeded = (93 * gallons - lower * gallons) / (higher - lower);
+    let lowerneeded = gallons - higherneeded;
+    document.getElementById("lowerneeded").innerHTML = lowerneeded.toFixed(2);
+    document.getElementById("higherneeded").innerHTML = higherneeded.toFixed(2);
+}
