a31bbe4cc954b76a946fb66eb225cfaa52ee3d4d
Tyr Zong <tianyizong@gmail.com>
2023-09-01 12:12:37 -0400

    toggle detail on list item

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,19 @@ function runCalc(){
     document.getElementById("lowerneeded").innerHTML = lowerneeded.toFixed(2);
     document.getElementById("higherneeded").innerHTML = higherneeded.toFixed(2);
 }
+
+function toggleItem(elm){
+    let hidden = elm.getElementsByClassName("hide");
+    if (hidden.length == 0){
+        let details = elm.getElementsByClassName("detail");
+        for (let i = 0; i < details.length; i++){
+            details[i].classList.add("hide");
+        }
+    }
+    else{
+        for (let i = 0; i < hidden.length; i++){
+            hidden[i].classList.remove("hide");
+        }
+        hidden[0].classList.remove("hide");
+    }
+}
